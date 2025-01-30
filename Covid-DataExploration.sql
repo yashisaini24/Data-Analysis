91a45dcafc8df444704d9ea1ca350d904449aa8f
apple <apple@apples-MacBook-Pro.local>
2025-01-30 19:06:52 +0100

    Initial commit

---
+++ Covid-DataExploration.sql
@@ -0,0 +1,90 @@
+Select *
+From Covid.CovidDeaths
+Where continent is not null 
+order by 3,4
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From Covid.CovidDeaths
+Where continent is not null 
+order by 1,2
+
+-- Total Cases vs Total Deaths
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From Covid.CovidDeaths
+Where location like '%India%'
+and continent is not null 
+order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid
+
+Select Location, date, Population, total_cases,  (total_cases/population)*100 as PercentPopulationInfected
+From covid.CovidDeaths
+Where location like '%India%'
+order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Covid.CovidDeaths
+Group by 1,2
+order by PercentPopulationInfected desc
+
+-- Countries with Highest Death Count per Population
+
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From Covid.CovidDeaths
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From Covid.CovidDeaths
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Covid.CovidDeaths
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(vac.new_vaccinations) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--(RollingPeopleVaccinated/population)*100 for this CTE is required - next code
+From Covid.CovidDeaths dea
+Join Covid.CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+With PopvsVac 
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(vac.new_vaccinations) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From Covid.CovidDeaths dea
+Join Covid.CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+) 
+Select *, (RollingPeopleVaccinated/Population)*100 as NewC
+From PopvsVac
